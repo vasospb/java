a62718227e026cb87144eeab4a3b64ffa1b2f70a
vasospb <v.chigaev@gmail.com>
2016-03-17 19:58:11 +0300

    ff

--- leaks/ClassLoaderLeakExample.java
+++ leaks/ClassLoaderLeakExample.java
@@ -4,7 +4,6 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.nio.file.Path;
 
-
 public final class ClassLoaderLeakExample {
     static volatile boolean running = true; // used to stop LongRunningThread
 
